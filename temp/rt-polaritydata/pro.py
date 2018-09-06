fa93e34de3fbe69a98c8e1576fb34eb14e4b9a21
cmwenliu <cmwenliu@gmail.com>
2018-09-06 15:58:03 -0500

    tested

--- temp/rt-polaritydata/pro.py
+++ temp/rt-polaritydata/pro.py
@@ -1,12 +1,15 @@
 import glob
 import os
+#import sys
+#reload(sys)
+#sys.setdefaultencoding('utf8')
 
-files = glob.glob('rt*')
+files = glob.glob('*.org')
 for f in files:
-	with open (f, 'r', encoding='ISO-8859-1') as inf:
+	with open (f, 'r', encoding = 'ISO-8859-1') as inf:
 		count = 1
 		for line in inf:
 			fname = str(count) + '_' + f + '.txt'
-			with open(fname, 'w', encoding='utf-8') as outf:
+			with open(fname, 'w', encoding = 'ISO-8859-1') as outf:
 				outf.write(line)
 			count += 1
